39fbc6def7d20fa9e7cafa1921737e07a5fd05cb
Nihar002561 <naineshgevariya@gmail.com>
2024-12-18 11:01:36 +0530

    profile page

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -7,9 +7,11 @@ function Profile() {
         Lastname: "",
         Username: "",
         Emailaddress: "",
+        Occupation: "",
         Gender: "",
         City: "",
     })
+
     function handleSubmit(){
         console.log(user);
     }
@@ -24,6 +26,7 @@ function Profile() {
   return (
     <form className="container col-md-6 border border-black p-5" noValidate>
         <h2 className='container col-md-6'>Profile Page</h2>
+        <img src="https://static.vecteezy.com/system/resources/previews/045/944/199/non_2x/male-default-placeholder-avatar-profile-gray-picture-isolated-on-background-man-silhouette-picture-for-user-profile-in-social-media-forum-chat-greyscale-illustration-vector.jpg" style={{width: '18rem'}} className="card-img-top border border-dark-subtle border-2" alt="..." />
         <div className="col-md-4">
             <label htmlFor="validationCustom01" className="form-label">First name</label>
 
@@ -62,6 +65,17 @@ function Profile() {
             onChange={(e) => handlechange(e)}
              className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
             {/* <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div> */}
+        </div>
+
+        <div className='col-md-4 mb-3'>
+            <label htmlFor="exampleFormControlInput1" className="form-label">Occupation</label>
+            <select className="form-select" name='Occupation'
+             value={user.Occupation} 
+             onChange={(e) => handlechange(e)}>
+                <option value="Ui-Ux">Ui-Ux</option>
+                <option value="Web-Devloper">Web-Devloper</option>
+                <option value="App-Devloper">App-Devloper</option>
+            </select>
         </div>
 
         <div>
